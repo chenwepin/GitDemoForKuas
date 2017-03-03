1b74177f6abca03aa86fe2d88fb5d885adc3b45d
chenwepin <1103137138@gm.kuas.edu.tw>
2017-03-03 14:18:21 +0800

    hahaha

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -16,7 +16,7 @@ namespace Hello
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1());//hahaha
         }
     }
 }
